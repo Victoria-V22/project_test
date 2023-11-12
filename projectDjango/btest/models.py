6a889e18a8136180d9bc151c97ced2a087db6ddd
Victoria-V22 <v.a.chekunova@gmail.com>
2023-11-12 20:12:26 +0300

    -

--- projectDjango/btest/models.py
+++ projectDjango/btest/models.py
@@ -7,3 +7,11 @@ class Bb(models.Model):
   content = models.TextField(null=True, blank=True)
   price = models.FloatField(null=True, blank=True)
   published = models.DateTimeField(auto_now_add=True, db_index=True)
+
+class Rubric(models.Model):
+  name = models.CharField(max_length=20, db_index=True,   verbose_name='Название')
+  
+  class Meta:
+    verbose_name_plural = 'Рубрики'
+    verbose_name = 'Рубрика'
+    ordering = ['name']
